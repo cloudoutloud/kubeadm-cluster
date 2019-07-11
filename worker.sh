82a67d541fc316834302a2f5deffb1fc13f0de52
Sean Rigby <seanrigby@seans-mbp-2.home>
2019-07-11 12:36:43 +0100

    Add sshpass command

--- worker.sh
+++ worker.sh
@@ -2,6 +2,11 @@
 
 use scp to retrive bootstrap file from master node
 
+# Join worker nodes to the cluster
+echo "Joining worker node to cluster, may take some time.."
+yum install -q -y sshpass >/dev/null 2>&1
+sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 192.168.0.30:/bootstrap_token.sh /bootstrap_token.sh 2>/dev/null
+bash /bootstrap_token.sh >/dev/null 2>&1
 
 #Add route to worker nodes for weaveworks
 ip route add 10.96.0.1/32 dev eth1
